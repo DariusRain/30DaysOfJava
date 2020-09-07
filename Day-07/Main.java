cfd9e7d7e72bdc8a83cd90c57fe3ae4c41915ab1
DariusRain <dariusrain.student@careerdevs.com>
2020-09-07 12:02:07 -0400

    Finished Day07 of Java

---
+++ Day-07/Main.java
@@ -0,0 +1,39 @@
+// Day 7: Arrays
+// https://www.hackerrank.com/challenges/30-arrays/copy-from/180461287
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+        String result = "";
+        int counter = n - 1;
+        while(counter != -1) {
+            result += arr[counter] + " ";
+            counter -= 1;
+        }
+        System.out.println(result);
+        scanner.close();
+    }
+}
