80de25283b9765ceb03ab09de061d7b3300eff7e
DariusRain <dariusrain.student@careerdevs.com>
2020-09-15 19:15:31 -0400

    Getting rid of unwanted classes.

--- bank/src/SavingsAccount.java
+++
@@ -1,8 +0,0 @@
-public class SavingsAccount {
-    private String dateOfCreation;
-    private String dateOfLastDeposit;
-    private String dateOfLastWithdraw;
-    private String amountOfLastDeposit;
-    private String amountOfLastWithdraw;
-    private int balance;
-}
